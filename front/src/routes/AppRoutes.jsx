846c2d0fd96c747c1d1008bf56e5197b9fd7d23d
Guilherme <guilhermesi2005z@gmail.com>
2026-05-07 13:41:56 -0300

    merge branchCriarTurma e branch development

--- front/src/routes/AppRoutes.jsx
+++ front/src/routes/AppRoutes.jsx
@@ -5,8 +5,8 @@ import Login from "../pages/login/Login";
 import Home from "../pages/Home/Home";
 import CadastrarAluno from "../pages/CadastrarAluno/CadastrarAluno";
 import EditarAluno from "../pages/EditarAluno/EditarAluno";
-import PerfilAluno from "../pages/PerfilAluno/PerfilAluno";
 import VisualizarTurmas from "../pages/Turmas/VisualizarTurmas";
+import PerfilAluno from "../pages/PerfilAluno/PerfilAluno";
 export default function AppRoutes(){
     return (
         <Routes>
@@ -15,6 +15,8 @@ export default function AppRoutes(){
                 <Route path="/" element={<Home/>} />
                 <Route path="/cadastrarAluno" element={<CadastrarAluno/>}></Route>
                 <Route path="/editarAluno" element={<EditarAluno/>} ></Route>
+                <Route path="/turmas" element={<VisualizarTurmas />} />
+                <Route path="/perfilAluno" element={<PerfilAluno/>} ></Route>
             </Route>
         </Routes>
     )
